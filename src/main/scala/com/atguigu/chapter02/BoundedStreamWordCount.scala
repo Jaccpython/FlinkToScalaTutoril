096d669307cdd27e9f2b9b9cfd63260f9ac43773
Jaccpython <1839841264@qq.com>
2023-08-13 18:31:02 +0800

    2023/8/13 有界流_1

---
+++ src/main/scala/com/atguigu/chapter02/BoundedStreamWordCount.scala
@@ -0,0 +1,30 @@
+package com.atguigu.chapter02
+
+import org.apache.flink.streaming.api.scala._
+
+object BoundedStreamWordCount {
+  def main(args: Array[String]): Unit = {
+
+    // 1. 创建一个流式执行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // 2. 读取文本文件数据
+    val lineDatStream: DataStream[String] = env.readTextFile("input/words.txt")
+
+    // 3. 对数据集进行转换处理
+    val wordAndOne: DataStream[(String, Int)] = lineDatStream.flatMap(_.split(" ")).map((_, 1))
+
+    // 4. 按照单词进行分组
+    val wordAndOneKey: KeyedStream[(String, Int), String] = wordAndOne.keyBy(_._1)
+
+    // 5. 对分组数据进行sum聚合统计
+    val sum = wordAndOneKey.sum(1)
+
+    // 6. 打印输出
+    sum.print()
+
+    // 7. 执行任务
+    env.execute()
+  }
+
+}
